f5274b0f7aadd0c70b897589f7da70d0e89957ee
Daniel Alberto Larios Torres <daniellarios@MacBook-Pro-de-Daniel.local>
2021-01-07 00:20:31 -0600

    other: added missing repository

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from '../users/user.entity';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [UsersModule, PassportModule, TypeOrmModule.forFeature([User])],
   providers: [AuthService, LocalStrategy]
 })
 export class AuthModule {}
