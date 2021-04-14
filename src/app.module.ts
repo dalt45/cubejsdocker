5974d457542976ac6e08813d02a9fd80534a1f23
Daniel Larios <d.larios@uniandes.edu.co>
2021-04-14 12:32:47 +0200

    other: changed settings (databaseName) for dev env

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,11 +21,9 @@ import { ApplicationEvent } from './events/events.entity';
 // 'mongodb://user:password@mongo:27017'
 const dbHost: string = process.env.DB_HOST;
 const dbPort: number = (process.env.DB_PORT as unknown) as number;
-// const dbUsername: string = process.env.DB_USERNAME;
-// const dbPassword: string = process.env.DB_PASSWORD;
-const dbName: string = process.env.DB_NAME;
 const dbUsername: string = process.env.DB_USERNAME;
 const dbPassword: string = process.env.DB_PASSWORD;
+const dbName: string = process.env.DB_NAME;
 
 const additionalProdSettings = () => {
   if (process.env.URL_DOMAIN === 'http://localhost:3000') {
@@ -47,7 +45,8 @@ const additionalProdSettings = () => {
       type: 'mongodb',
       host: dbHost,
       port: dbPort,
-      ...additionalProdSettings(),
+      username: dbUsername,
+      password: dbPassword,
       entities: [
         User,
         Admin,
